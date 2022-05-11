769a8a19abe85ab951e346add5bd7a86663f76ef
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2022-05-11 21:05:04 +0700

    add Portfolio section

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
+import Project from '../Project/Project';
+import './Portfolio.css';
+import { projects } from '../../data';
 
 const Portfolio = () => {
-  return <div id="portfolio">Portfolio</div>;
+  return (
+    <section className="section portfolio" id="portfolio">
+      <div className="section-center">
+        <div className="section-title portfolio-title">
+          <h2>Portfolio</h2>
+          <div className="underline"></div>
+        </div>
+
+        {projects.map(project => {
+          return <Project project={project} key={project.id} />;
+        })}
+      </div>
+      <div className="more-btn">
+        <a
+          href="https://github.com/namnguyen-dev"
+          className="btn project-btn"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          more project
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Portfolio;
---
+++ src/components/Project/Project.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import './Project.css';
+
+const Project = ({ project }) => {
+  const { img, title, description, tags, liveLink, ghLink, id } = project;
+  return (
+    <div className={`project ${id % 2 === 0 ? 'even-project' : ''}`}>
+      <div className="project-image">
+        <a href={liveLink} target="_blank" rel="noopener noreferrer">
+          <img src={img} alt={title} />
+        </a>
+      </div>
+
+      <div className="project-content">
+        <h3 className="project-title">
+          <a href={liveLink} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h3>
+        <div className="project-description">
+          <p>{description}</p>
+        </div>
+        <ul className="project-tags">
+          {tags.map((tag, index) => (
+            <li className="tag" key={index}>
+              {tag}
+            </li>
+          ))}
+        </ul>
+        <div className="project-links">
+          <a
+            href={liveLink}
+            className={`btn project-btn`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            live
+          </a>
+          <a
+            href={ghLink}
+            className={`btn project-btn`}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            source
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
