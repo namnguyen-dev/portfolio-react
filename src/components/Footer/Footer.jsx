7ce0ebd33677a451f4dc2297dce0e1013aa7131e
Nam Nguyen <namnguyenthanh.developer@gmail.com>
2022-05-12 10:33:01 +0700

    add Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import SocialLinks from '../Navbar/SocialLinks/SocialLinks';
+import './Footer.css';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer">
+      <div className="section-center">
+        <SocialLinks />
+        <p>
+          &copy; <span>{new Date().getFullYear()}</span> Nam Nguyen. all rights
+          reserved
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
